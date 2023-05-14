b7c822f6cfde10e5b626df2700ead3d0e69294e1
Kit Keen <workpav17@gmail.com>
2023-05-14 11:00:17 +0300

    brocker

--- MessageHandlers/MessageBrocker.cs
+++ MessageHandlers/MessageBrocker.cs
@@ -1,5 +1,7 @@
+using System.Text;
 using CoffieNanny.Extentions;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace nannyservice;
 
@@ -12,7 +14,11 @@ public static class MessageBrocker
 
     public static void ConnectToBrocker(string user, string password, string hostName, string port, string vhost)
     {
-        ConnectionFactory factory = new ConnectionFactory();
+        ConnectionFactory factory = new ConnectionFactory
+        {
+            AutomaticRecoveryEnabled = true,
+            NetworkRecoveryInterval = TimeSpan.FromSeconds(10),
+        };
 
         if (user.IsNullOrWhitespace())
             user = "guest";
@@ -31,10 +37,32 @@ public static class MessageBrocker
         
         factory.Uri = new Uri($"amqp://{user}:{password}@{hostName}:{port}/{vhost}");
 
+        var exchangeName = "FirstExchange";
+        var queueName = "FirstQueue";
+        var routingKey = "simple_example_of_routing_key";
+
         using IConnection connection = factory.CreateConnection();
-        using (IModel channel = connection.CreateModel())
+        using IModel channel = connection.CreateModel();
+        channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+        channel.QueueDeclare(queueName, false, false, false, null);
+        channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+        byte[] messageFromBody = "Hello world"u8.ToArray();
+        IBasicProperties props = channel.CreateBasicProperties();
+        props.ContentType = "text/plain";
+        props.DeliveryMode = 2;
+        props.Headers.Add("latitude",  51.5252949);
+        props.Headers.Add("longitude", -0.0905493);
+        props.Expiration = "36000000";
+        channel.BasicPublish(exchangeName, routingKey, null, messageFromBody);
+        
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (ch, ea) =>
         {
-            
-        }
+            var body = ea.Body.ToArray();
+            channel.BasicAck(ea.DeliveryTag, false);
+        };
+
+        string consumerTag = channel.BasicConsume(queueName, false, consumer);
     }
 }
